7150f891f7b49c59f96a2ef95262f1da6caffa49
David Klostermann <123davidLP@gmail.com>
2022-10-02 11:43:02 +0200

    test

--- .github/workflows/preprocessor.py
+++ .github/workflows/preprocessor.py
@@ -1,10 +1,43 @@
 import os
+import subprocess
 
 def set_env(name, value):
-    env_file = os.getenv('GITHUB_ENV')
+    env_file = os.getenv("GITHUB_ENV")
     with open(env_file, "a") as f:
         f.write(f"{name}=\"{value}\"")
 
+tag = os.getenv("GITHUB_REF").replace("refs/tags/", "")
+print(f"Found tag '{tag}'")
 
+raw_commits = subprocess.check_output(["git", "log", f"{tag}..main", "--oneline", "--no-decorate"])
 
-print("Ref: " + os.getenv('GITHUB_REF'))
+commits = { "fix": [], "feature": [], "tweak": [] }
+
+for line in raw_commits.split():
+    lowered = line.lower().strip()
+
+    if lowered.startswith("[fix]"):
+        commits["fix"] = line[len("[fix]"):].strip()
+    elif lowered.startswith("[feature]"):
+        commits["feature"] = line[len("[feature]"):].strip()
+    elif lowered.startswith("[tweak]"):
+        commits["tweak"] = line[len("[tweak]"):].strip()
+
+result_markdown = ""
+
+result_markdown += "Features Added:\n"
+for commit in commits["feature"]:
+    result_markdown += f"*) {commit}\n"
+result_markdown += "\n"
+
+result_markdown += "Tweaks:\n"
+for commit in commits["tweak"]:
+    result_markdown += f"*) {commit}\n"
+result_markdown += "\n"
+
+result_markdown += "Fixes:\n"
+for commit in commits["fix"]:
+    result_markdown += f"*) {commit}\n"
+result_markdown += "\n"
+
+print(result_markdown)
